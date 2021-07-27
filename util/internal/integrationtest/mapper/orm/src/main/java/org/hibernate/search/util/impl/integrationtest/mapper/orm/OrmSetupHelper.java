01312d1fd17a5050b24ec9f5fc41635c5efd276c
Yoann Rodière <yoann@hibernate.org>
2021-07-27 11:36:52 +0200

    DONOTMERGE Disable asynchronous indexing tests
    They rely on waiting for assertions, and since most of these assertions
    fail, they wait a lot. Let's re-enable them when assertions no longer
    fail in the base (synchronous indexing) tests.

--- util/internal/integrationtest/mapper/orm/src/main/java/org/hibernate/search/util/impl/integrationtest/mapper/orm/OrmSetupHelper.java
+++ util/internal/integrationtest/mapper/orm/src/main/java/org/hibernate/search/util/impl/integrationtest/mapper/orm/OrmSetupHelper.java
@@ -7,6 +7,7 @@
 package org.hibernate.search.util.impl.integrationtest.mapper.orm;
 
 import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -126,6 +127,8 @@ public final class OrmSetupHelper
 		for ( BackendMock backendMock : backendMocks ) {
 			backendMock.indexingWorkExpectations( coordinationStrategyExpectations.indexingWorkExpectations );
 		}
+		assumeTrue( "Asynchronous indexing tests are temporarily disabled",
+				coordinationStrategyExpectations.sync );
 	}
 
 	@Override
