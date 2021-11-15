16bbebd005f20af160e8f22d07f6e969b2e66110
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2021-11-15 16:12:03 +0100

    TODOCHECK Avoid to reuse query instances

--- mapper/orm/src/main/java/org/hibernate/search/mapper/orm/loading/impl/HibernateOrmSelectionEntityByIdLoader.java
+++ mapper/orm/src/main/java/org/hibernate/search/mapper/orm/loading/impl/HibernateOrmSelectionEntityByIdLoader.java
@@ -40,7 +40,6 @@ class HibernateOrmSelectionEntityByIdLoader<E> extends AbstractHibernateOrmSelec
 		List<E> loadedEntities = createListContainingNulls( allIds.size() );
 
 		int fetchSize = loadingOptions.fetchSize();
-		Query<E> query = createQuery( fetchSize, timeout );
 
 		List<Object> ids = new ArrayList<>( fetchSize );
 		for ( int i = 0; i < keys.length; i++ ) {
@@ -56,6 +55,7 @@ class HibernateOrmSelectionEntityByIdLoader<E> extends AbstractHibernateOrmSelec
 
 			ids.add( key.getIdentifier() );
 			if ( ids.size() >= fetchSize ) {
+				Query<E> query = createQuery( fetchSize, timeout );
 				query.setParameterList( IDS_PARAMETER_NAME, ids );
 				// The result is worthless, as entities are not in the right order.
 				// However, this will load entities into the persistence context... see further down.
@@ -64,6 +64,7 @@ class HibernateOrmSelectionEntityByIdLoader<E> extends AbstractHibernateOrmSelec
 			}
 		}
 		if ( !ids.isEmpty() ) {
+			Query<E> query = createQuery( fetchSize, timeout );
 			query.setParameterList( IDS_PARAMETER_NAME, ids );
 			// Same as above: the result is worthless.
 			query.getResultList();
